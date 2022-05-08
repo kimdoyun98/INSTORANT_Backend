f15afbcb53bba1801183673451129258fc708108
kimdoyun98 <rladlsxo1250@gmail.com>
2022-05-08 22:12:16 +0900

    JWT urls 추가

--- Restaurant_recommend/urls.py
+++ Restaurant_recommend/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Sign.urls')),
+    path('api/token/', obtain_jwt_token),  # JWT 토큰을 발행할 때 사용
+    path('api/token/verify/', verify_jwt_token),  # JWT 토큰이 유효한 지 검증
+    path('api/token/refresh/', refresh_jwt_token),  # JWT 토큰을 갱신할 때 사용
 ]
