44409d762a88ca2e718ea658e77c0dd98753adf3
kimdoyun98 <rladlsxo1250@gmail.com>
2022-05-08 23:57:12 +0900

    수정

--- Sign/views.py
+++ Sign/views.py
@@ -3,7 +3,9 @@ from django.contrib.auth import logout
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import User_Infomation
-
+from rest_framework.decorators import permission_classes, authentication_classes, api_view
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 # class SignIn(APIView):
 #     def post(self, request):
@@ -27,12 +29,12 @@ from .models import User_Infomation
 #         logout(request)
 #         return Response({'msg': '로그아웃'})
 
-
+@permission_classes([AllowAny])
 class SignUp(APIView):
     def post(self, request):
-        username = request.data.get("user_id")
-        user_pw = request.data.get("user_pw")
-        user = User_Infomation.objects.create_user(username=username, password=user_pw)
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = User_Infomation.objects.create_user(username=username, password=password)
         user.save()
 
         return Response({'msg': '등록 완료'})
