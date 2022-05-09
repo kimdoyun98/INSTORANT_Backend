d0c53ccebe93ffefbf1d38f25e942aec61303f73
kimdoyun98 <rladlsxo1250@gmail.com>
2022-05-09 15:54:05 +0900

    Update urls.py

--- Sign/urls.py
+++ Sign/urls.py
@@ -1,17 +1,7 @@
 from django.urls import path
-<<<<<<< Updated upstream
-from .views import SignUp, SignIn, SignOut
+from .views import SignUp
+
 
 urlpatterns = [
     path('signup/', SignUp.as_view()),
-    path('signin/', SignIn.as_view()),
-    path('signout/', SignOut.as_view()),
-=======
-from .views import SignUp # , SignIn, SignOut
-
-urlpatterns = [
-    path('signup/', SignUp.as_view()),
-    #path('signin/', SignIn.as_view()),
-    #path('signout/', SignOut.as_view()),
->>>>>>> Stashed changes
 ]
