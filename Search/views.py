8b7b6875c252a1f199456ad38badc077cbe6b980
kimdoyun98 <rladlsxo1250@gmail.com>
2022-05-10 19:52:10 +0900

    add

--- Search/views.py
+++ Search/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.contrib.auth import logout
+from rest_framework.response import Response
+from rest_framework.views import APIView
+import pymongo
+from rest_framework.decorators import permission_classes, authentication_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
-# Create your views here.
+
+@permission_classes([AllowAny])#권한체크
+class Search_Title (APIView):
+    def Post(self, request):
+        pass
