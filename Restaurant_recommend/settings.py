24a02d91f46deeaeb62a4985acaa5f86402f80ca
kimdoyun98 <rladlsxo1250@gmail.com>
2022-06-21 20:55:48 +0900

    Update settings.py

--- Restaurant_recommend/settings.py
+++ Restaurant_recommend/settings.py
@@ -104,6 +104,7 @@ DATABASES = {
         'PASSWORD': config['SECRET']['PASSWORD'],
         'HOST': config['SECRET']['HOST'],
         'PORT': config['SECRET']['PORT'],
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
 
