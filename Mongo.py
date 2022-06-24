34de9190d592832898e62a2dd26ed10e5bc55470
kimdoyun98 <rladlsxo1250@gmail.com>
2022-06-24 10:01:14 +0900

    Update Mongo.py

--- Mongo.py
+++ Mongo.py
@@ -1,5 +1,5 @@
 import pymongo
 
-myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+myclient = pymongo.MongoClient('mongodb://175.106.96.235:27017/')
 mydb = myclient['Test']
 mycol = mydb['test']
