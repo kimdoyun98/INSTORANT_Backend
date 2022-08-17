eb54b178eedb9d57af9936ad018dfa8078d2f806
kimdoyun98 <rladlsxo1250@gmail.com>
2022-08-17 23:26:19 +0900

    코드 및 주석 정리

--- Detail_info/urls.py
+++ Detail_info/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Detail, Favorite
+from .views import Detail, Favorite, Favorite_List
 
 urlpatterns = [
     path('data/', Detail.as_view()),
     path('favorite/', Favorite.as_view()),
+    path('favorite_list/', Favorite_List.as_view()),
 ]
