e6afef3ddda7b808deb7668023639b4b45b75dec
kimdoyun98 <rladlsxo1250@gmail.com>
2022-08-24 22:59:11 +0900

    헤더 정보로 확인할 수 있는 것 수정

--- Detail_info/views.py
+++ Detail_info/views.py
@@ -19,11 +19,18 @@ from Sign.models import UserFavorite, UserInfomation, UserReview
 # 상세정보 API
 class Detail (APIView):
     def post(self, request):
-        mongo_id = request.data.get('mongo_id')
-        username = request.data.get("username")
-        user_fav = UserFavorite.objects.filter(username=username, favor_id=mongo_id).exists()
+        restaurant_id = request.data.get('restaurant_id')
+        access_token = request.headers.get('Authorization', None)
 
-        restaurant = mycol.find_one({'_id': mongo_id},
+        if access_token == "":
+            username = None
+        else:
+            payload = jwt.decode(access_token, 'secret', algorithm='HS256')
+            username = payload["username"]
+
+        user_fav = UserFavorite.objects.filter(username=username, favor_id=restaurant_id).exists()
+
+        restaurant = mycol.find_one({'_id': restaurant_id},
                                     {'_id': 1, 'Address': 1, "Name": 1, "Image": 1,
                                      "Score": 1, 'Tell_number': 1, 'Tag': 1})
 
@@ -72,30 +79,8 @@ class Random (APIView):
 
 # 즐겨찾기 추기&삭제
 class Favorite (APIView):
-    @login_check  # Token 인증
-    def put(self, request):  # 추가
-        username = request.data.get("username")
-        mongo_id = request.data.get("mongo_id")
-
-        user = UserInfomation.objects.get(username=username)
-        UserFavorite.objects.create(username=user, favor_id=mongo_id)
-
-        return Response(status=200)
-
     @login_check
-    def post(self, request):  # 삭제
-        username = request.data.get("username")
-        mongo_id = request.data.get("mongo_id")
-
-        UserFavorite.objects.get(username=username, favor_id=mongo_id).delete()
-
-        return Response(status=200)
-
-
-# 즐겨찾기 목록 API
-class Favorite_List(APIView):
-    @login_check
-    def post(self, request):
+    def get(self, request):
         access_token = request.headers.get('Authorization', None)
         payload = jwt.decode(access_token, 'secret', algorithm='HS256')
         fav_ids = UserFavorite.objects.filter(username=payload['username'])
@@ -103,10 +88,42 @@ class Favorite_List(APIView):
         restaurant_list = []
         for fav_id in fav_ids:
             restaurant_list.append(mycol.find_one({"_id": fav_id.favor_id},
-                                            {"_id": 1, "Name": 1, "Image": 1, "Score": 1}))
+                                                  {"_id": 1, "Name": 1, "Image": 1, "Score": 1}))
 
-        print(restaurant_list)
         return Response({'data': restaurant_list})
+
+    @login_check  # Token 인증
+    def put(self, request):  # 추가
+        restaurant_id = request.data.get("restaurant_id")
+
+        access_token = request.headers.get('Authorization', None)
+
+        if access_token == "":
+            username = None
+        else:
+            payload = jwt.decode(access_token, 'secret', algorithm='HS256')
+            username = payload["username"]
+
+        user = UserInfomation.objects.get(username=username)
+        UserFavorite.objects.create(username=user, favor_id=restaurant_id)
+
+        return Response(status=200)
+
+    @login_check
+    def post(self, request):  # 삭제
+        restaurant_id = request.data.get("restaurant_id")
+
+        access_token = request.headers.get('Authorization', None)
+
+        if access_token == "":
+            username = None
+        else:
+            payload = jwt.decode(access_token, 'secret', algorithm='HS256')
+            username = payload["username"]
+
+        UserFavorite.objects.get(username=username, favor_id=restaurant_id).delete()
+
+        return Response(status=200)
 
 
 # 리뷰(댓글) 조회, 삽입, 삭제
